a0f58ac8ac7675915ba989f818a03690d9a306b6
IngvarAtli <iaa10@hi.is>
2024-09-09 15:10:41 +0000

    lagaði aðeins lidur1.py þar sem það voru óþarfa útskýringar á segðum í skjalinu.

--- lidur1.py
+++ lidur1.py
@@ -6,24 +6,7 @@ def find_kennitolur(file_path, types):
         text = file.read()
 
     einstaklingskenn = r'\b[0-3]\d(0\d|1[0-2])\d{2}-[2-9]\d{2}[8,9,0]'
-    """
-    [0-3] velur fyrsta tölustaf sem getur bara verið á bilinu 0 upp í 3
-    \d{5} sex tölustafir á bilinu 0-9
-    - strik milli fyrstu 6 og seinni 4 tölustöfum
-
-    """
-    fyrirtaekjakenn = r'\b[4-9]\d{5}-\d{4}\b'
-
-    """
-    Útskýring fyrir reglulega segð (fyrirtækja):
-    \b þýðir byrjun á orði
-    \d[4-9] velur tölustaf á bilinu 4 upp í 9
-    \d{5} velur 5 tölustafi á bilinu 0-9
-    - strik milli fyrstu 6 og seinni 4 tölustafa í kennitölu
-    \d{4} velur 4 tölustafi á bilinu 0-9
-    \b táknar svo enda orðs (eða talnaröð í þessu tilfelli)
-    """
-    
+    fyrirtaekjakenn = r'\b[4-9]\d{5}-\d{4}\b'    
     if 'einstaklingar' in types:
         print("Einstaklingar:")
         for match in re.finditer(einstaklingskenn, text):
